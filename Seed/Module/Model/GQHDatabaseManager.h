a9cea18c0af35321fd89ff76ef90f026ec76de68
GuanQinghao <qinghaoguan@163.com>
2019-12-02 18:49:32 +0800

    sqlite

--- Seed/Module/Model/GQHDatabaseManager.h
+++ Seed/Module/Model/GQHDatabaseManager.h
@@ -16,6 +16,86 @@ NS_ASSUME_NONNULL_BEGIN
 /// 数据库管理单例
 + (instancetype)qh_sharedDatabaseManager;
 
+//MARK:数据库操作
+/// 创建数据库文件
+/// @param databaseName 数据库文件名
+/// @param path 数据库文件路径
+- (BOOL)qh_createDatabase:(NSString *)databaseName atPath:(NSString *)path;
+
+/// 删除数据库文件
+/// @param databaseName 数据库文件名
+/// @param path 数据库文件路径
+- (BOOL)qh_removeDatabase:(NSString *)databaseName atPath:(NSString *)path;
+
+/// 数据库文件路径(已存在的数据库,不存在则返回nil)
+/// @param databaseName 数据库文件名
+- (nullable NSString *)qh_pathOfDatabase:(NSString *)databaseName;
+
+//MARK:数据表操作
+/// 创建数据表
+/// @param databaseName 数据库文件名
+/// @param tableName 数据表名
+/// @param cls 数据表对应的模型类
+/// @param primary 主键名(单主键)
+- (BOOL)qh_createTableInDatabase:(NSString *)databaseName withTableName:(NSString *)tableName model:(Class)cls primary:(NSString *)primary;
+
+/// 清空数据库中的数据表
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (BOOL)qh_truncateTable:(NSString *)tableName inDatabase:(NSString *)databaseName;
+
+/// 删除数据库中的数据表
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (BOOL)qh_dropTable:(NSString *)tableName inDatabase:(NSString *)databaseName;
+
+//MARK:CRUD
+/// 插入数据
+/// @param model 数据模型
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (BOOL)qh_insertData:(id)model inTable:(NSString *)tableName database:(NSString *)databaseName;
+
+/// 查询指定属性指定值的数据
+/// @param cls 数据表对应的模型类
+/// @param key 指定属性
+/// @param value 指定值
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (NSArray *)qh_queryData:(Class)cls withKey:(NSString *)key value:(NSString *)value inTable:(NSString *)tableName database:(NSString *)databaseName;
+
+/// 查询数据表中的所有数据
+/// @param cls 数据表对应的模型类
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (NSArray *)qh_queryAllWithClass:(Class)cls inTable:(NSString *)tableName database:(NSString *)databaseName;
+
+/// 删除指定属性指定值的数据
+/// @param cls 数据表对应的模型类
+/// @param key 指定属性
+/// @param value 指定值
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (NSArray *)qh_deleteData:(Class)cls withKey:(NSString *)key value:(NSString *)value inTable:(NSString *)tableName database:(NSString *)databaseName;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /// 插入数据
 /// @param model 模型数据
 /// @param path 数据表路径
--- Seed/Module/Model/GQHDatabaseManager.m
+++ Seed/Module/Model/GQHDatabaseManager.m
@@ -9,15 +9,19 @@
 #import "GQHDatabaseManager.h"
 #import <FMDB/FMDB.h>
 #import <objc/message.h>
-#import <YYModel/YYModel.h>
 
 
 @interface GQHDatabaseManager ()
 
 /**
+ 文件管理器
+ */
+@property (nonatomic, strong) NSFileManager *fileManager;
+
+/**
  数据库队列
  */
-@property (nonatomic, strong) FMDatabaseQueue *dbQueue;
+@property (nonatomic, strong) FMDatabaseQueue *databaseQueue;
 
 /**
  数据表名
@@ -26,9 +30,299 @@
 
 @end
 
+static GQHDatabaseManager *manager = nil;
+
 @implementation GQHDatabaseManager
 
-static GQHDatabaseManager *manager = nil;
+/// 数据库管理单例
++ (instancetype)qh_sharedDatabaseManager {
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        manager = [[super allocWithZone:NULL] init];
+    });
+    
+    return manager;
+}
+
++ (instancetype)allocWithZone:(struct _NSZone *)zone {
+    
+    return [[self class] qh_sharedDatabaseManager];
+}
+
+//MARK:数据库操作
+/// 创建数据库文件
+/// @param databaseName 数据库文件名
+/// @param path 数据库文件路径
+- (BOOL)qh_createDatabase:(NSString *)databaseName atPath:(NSString *)path {
+    
+    if (databaseName && databaseName.length > 0) {
+        
+        // 数据库文件全路径
+        NSString *databasePath = [path stringByAppendingPathComponent:databaseName];
+        
+        if (![self.fileManager fileExistsAtPath:databasePath]) {
+            
+            // 创建数据库文件
+            return [self.fileManager createFileAtPath:databasePath contents:nil attributes:nil];
+        }
+    }
+    
+    return NO;
+}
+
+/// 删除数据库文件(不能删除非空数据库)
+/// @param databaseName 数据库文件名
+/// @param path 数据库文件路径
+- (BOOL)qh_removeDatabase:(NSString *)databaseName atPath:(NSString *)path {
+    
+    if (databaseName && databaseName.length > 0) {
+        
+        // 数据库文件全路径
+        NSString *databasePath = [path stringByAppendingPathComponent:databaseName];
+        
+        if ([self.fileManager fileExistsAtPath:databasePath]) {
+            
+            // 数据库队列
+            self.databaseQueue = [FMDatabaseQueue databaseQueueWithPath:databasePath];
+            // 数据表名
+            __block NSString *tableNames = @"";
+            
+            if (self.databaseQueue) {
+                
+                [self.databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
+                    
+                    FMResultSet *resultSet = [db executeQuery: [self sql_queryAllTableName]];
+                    
+                    while ([resultSet next]) {
+                        
+                        [[resultSet resultDictionary] enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+                            
+                            tableNames = [tableNames stringByAppendingFormat:@" %@", obj];
+                        }];
+                    }
+                }];
+                
+                if (tableNames.length > 0) {
+                    
+                    NSLog(@"table name:%@",tableNames);
+                    
+                    return NO;
+                }
+            }
+            
+            return [self.fileManager removeItemAtPath:databasePath error:nil];
+        }
+    }
+    
+    return NO;
+}
+
+/// 数据库文件路径(已存在的数据库,不存在则返回nil)
+/// @param databaseName 数据库文件名
+- (NSString *)qh_pathOfDatabase:(NSString *)databaseName {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return nil;
+}
+
+/// 遍历路径, 找到文件名
+/// @param path 路径
+- (void)retrieveFileName:(NSString *)fileName atPath:(NSString *)path {
+    
+    // 是否是文件夹目录
+    BOOL isDirectory = false;
+    // 是否存在
+    BOOL isExist = [self.fileManager fileExistsAtPath:path isDirectory:&isDirectory];
+    
+    if (isExist) {
+        
+        if (isDirectory) {
+            
+            // 文件夹下的所有文件及文件夹
+            NSArray *directoryArray = [self.fileManager contentsOfDirectoryAtPath:path error:nil];
+            
+            for (NSString *name in directoryArray) {
+                
+                // 忽略隐藏的文件或文件夹
+                if ([name hasPrefix:@"."]) {
+                    
+                    continue;
+                }
+                
+                // 递归遍历
+                [self retrieveFileName:fileName atPath:[path stringByAppendingPathComponent:name]];
+            }
+        } else {
+            
+            NSArray<NSString *> *components = [path componentsSeparatedByString:@"/"];
+            if ([[components lastObject] isEqualToString:fileName]) {
+                
+            }
+        }
+    } else {
+        
+        NSLog(@"file not exist!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//MARK:数据表操作
+/// 创建数据表
+/// @param databaseName 数据库文件名
+/// @param tableName 数据表名
+/// @param cls 数据表对应的模型类
+/// @param primary 主键名(单主键)
+- (BOOL)qh_createTableInDatabase:(NSString *)databaseName withTableName:(NSString *)tableName model:(Class)cls primary:(NSString *)primary {
+    
+}
+
+/// 清空数据库中的数据表
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (BOOL)qh_truncateTable:(NSString *)tableName inDatabase:(NSString *)databaseName {
+    
+}
+
+/// 删除数据库中的数据表
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (BOOL)qh_dropTable:(NSString *)tableName inDatabase:(NSString *)databaseName {
+    
+}
+
+//MARK:CRUD
+/// 插入数据
+/// @param model 数据模型
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (BOOL)qh_insertData:(id)model inTable:(NSString *)tableName database:(NSString *)databaseName {
+    
+}
+
+/// 查询指定属性指定值的数据
+/// @param cls 数据表对应的模型类
+/// @param key 指定属性
+/// @param value 指定值
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (NSArray *)qh_queryData:(Class)cls withKey:(NSString *)key value:(NSString *)value inTable:(NSString *)tableName database:(NSString *)databaseName {
+    
+}
+
+/// 查询数据表中的所有数据
+/// @param cls 数据表对应的模型类
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (NSArray *)qh_queryAllWithClass:(Class)cls inTable:(NSString *)tableName database:(NSString *)databaseName {
+    
+}
+
+/// 删除指定属性指定值的数据
+/// @param cls 数据表对应的模型类
+/// @param key 指定属性
+/// @param value 指定值
+/// @param tableName 数据表名
+/// @param databaseName 数据库文件名
+- (NSArray *)qh_deleteData:(Class)cls withKey:(NSString *)key value:(NSString *)value inTable:(NSString *)tableName database:(NSString *)databaseName {
+    
+}
+
+
+
+
+//MARK:SQLite
+
+/// 查询所有数据表名
+- (NSString *)sql_queryAllTableName {
+    
+    return @"SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY name;";
+}
+
+
+
+/// SQL语句-创建数据表
+/// @param tableName 数据表名
+- (NSString *)sql_createTable:(NSString *)tableName {
+    
+    return [NSString stringWithFormat:@"CREATE TABLE IF NOT EXISTS '%@' ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'json' TEXT)",table];
+}
+
+
+/// 创建数据表
+/// @param table 数据表名称
+- (NSString *)sql_createTable:(NSString *)table {
+    
+    return [NSString stringWithFormat:@"CREATE TABLE IF NOT EXISTS '%@' ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 'json' TEXT)",table];
+}
+
+/// 插入一条数据
+/// @param json 数据
+/// @param table 数据表名称
+- (NSString *)sql_insertJSON:(NSString *)json table:(NSString *)table {
+    
+    return [NSString stringWithFormat:@"INSERT INTO '%@' (json) VALUES ('%@')", table, json];
+}
+
+//MARK:需要获取是哪一个？？
+/// 更新指定数据
+/// @param json 更新后的数据
+/// @param index 指定索引(id)
+/// @param table 数据表名称
+- (NSString *)sql_updateJSON:(NSString *)json index:(NSInteger)index table:(NSString *)table {
+    
+    return [NSString stringWithFormat:@"UPDATE '%@' SET JSON = '%@' WHERE ID = %ld;",table, json, index];
+}
+
+/// 查询所有数据
+/// @param table 数据表名称
+- (NSString *)sql_queryAllDataFrom:(NSString *)table {
+    
+    return [NSString stringWithFormat:@"SELECT * FROM '%@'",table];
+}
+
+/// 删除数据表
+/// @param table 数据表名称
+- (NSString *)sql_dropTable:(NSString *)table {
+    
+    return [NSString stringWithFormat:@"DROP TABLE '%@'", table];
+}
+
+
+
+
+
+
+
+
+
+
+
+//MARK:Setter/Getter
+- (NSFileManager *)fileManager {
+    
+    return [NSFileManager defaultManager];
+}
+
+
+
 
 //MARK: PublicMethod
 
@@ -45,7 +339,7 @@ static GQHDatabaseManager *manager = nil;
     __block BOOL result;
     
     // 数据表操作
-    [self.dbQueue inDatabase:^(FMDatabase * _Nonnull db) {
+    [self.databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
         
         if ([db open]) {
             
@@ -79,13 +373,13 @@ static GQHDatabaseManager *manager = nil;
 - (NSArray *)qh_fetchDataWithClass:(Class)cls filePath:(NSString *)path {
     
     // 数据库队列
-    self.dbQueue = [FMDatabaseQueue databaseQueueWithPath:path];
+    self.databaseQueue = [FMDatabaseQueue databaseQueueWithPath:path];
     // 数据表名称
     self.tableName = [[path componentsSeparatedByString:@"/"] lastObject];
     
     NSMutableArray *result = [NSMutableArray array];
     
-    [self.dbQueue inDatabase:^(FMDatabase * _Nonnull db) {
+    [self.databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
         
         if ([db open]) {
             
@@ -122,13 +416,13 @@ static GQHDatabaseManager *manager = nil;
 - (BOOL)qh_removeDatabase:(NSString *)path {
     
     // 数据库队列
-    self.dbQueue = [FMDatabaseQueue databaseQueueWithPath:path];
+    self.databaseQueue = [FMDatabaseQueue databaseQueueWithPath:path];
     // 数据表名称
     self.tableName = [[path componentsSeparatedByString:@"/"] lastObject];
     
     __block BOOL result;
     
-    [self.dbQueue inDatabase:^(FMDatabase * _Nonnull db) {
+    [self.databaseQueue inDatabase:^(FMDatabase * _Nonnull db) {
         
         if ([db open]) {
             
@@ -144,23 +438,6 @@ static GQHDatabaseManager *manager = nil;
     return result;
 }
 
-/// 数据库管理单例
-+ (instancetype)qh_sharedDatabaseManager {
-    
-    
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        
-        manager = [[super allocWithZone:NULL] init];
-    });
-    
-    return manager;
-}
-
-+ (instancetype)allocWithZone:(struct _NSZone *)zone {
-    
-    return [[self class] qh_sharedDatabaseManager];
-}
 
 //MARK: PrivateMethod
 /// 创建数据表
